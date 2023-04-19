05e327c11bebd5122ad6a47ee53b80891d0971fd
Nima <nima.mohamed@hotmail.com>
2023-04-19 11:04:53 -0700

    add accept button

--- client/src/components/Handyperson.js
+++ client/src/components/Handyperson.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
 import "./handyperson.css";
 
 const Handyperson = () => {
   const [todos, setTodos] = useState([]);
+  
 
   const getDataBase = async (e) => {
     try {
-      const response = await fetch("http://localhost:5000/hp/:id");
+      const response = await fetch(`http://localhost:5000/hp/${1}`);
       const jsonData = await response.json();
-      setTodos(jsonData)
+      setTodos(jsonData);
+      
     } catch (err) {
       console.log(err.message)
     }
   };
-
+  
   useEffect(() => {
     getDataBase();
   }, [])
 
-  const handleAccept = (orderId) => {
-    console.log(`Accepted order with ID: ${orderId}`);
+  const handleAccept = async (orderId) => {
+    // Update the status of the todo object
+    const updatedTodos = todos.map(todo => {
+      if (todo.order_id === orderId) {
+        return { ...todo, status: "Done" };
+      }
+      return todo;
+    });
+    
+    setTodos(updatedTodos);
+
   };
 
   return (
     <div>
       <h1>Handyperson</h1>
-      <button className="history"><a href="/handyperson_history">History</a></button>
+      <button className="history"><Link to="/handyperson_history">History</Link></button>
       <button className="Logout" onClick={() => window.location = "/#menu"}>
         Logout
       </button>
@@ -46,20 +58,20 @@ const Handyperson = () => {
         </thead>
         <tbody>
           {todos.map(item => (
-            <tr key={item.order_id}>
-              <td>{item.customers.first_name}</td>
-              <td>{item.customers.last_name}</td>
-              <td>{item.skills.skill_name}</td>
-              <td>{item.tasks.create_time}</td>
-              <td>{item.skills.price}</td>
-              <td>{item.tasks.description}</td>
-              <td>{item.tasks.address}</td>
-              <td>{item.customers.email}</td>
-              <td className={item.orders.status === "Done" ? "done" : ""}>
-                {item.orders.status}
+            <tr key={item.order_id}> 
+              <td> {item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.skill_name}</td>
+              <td>{item.create_time}</td>
+              <td>${item.price}</td>
+              <td>{item.description}</td>
+              <td>{item.address}</td>
+              <td>{item.email}</td>
+              <td className={item.status === "Done" ? "done" : ""}>
+                {item.status}
               </td>
               <td>
-                <button className="accept" onClick={() => handleAccept(item.order_id)}>Accept</button>
+              <button className="accept" onClick={() => handleAccept(item.order_id)}>Accept</button>
               </td>
             </tr>))}
         </tbody>
--- client/src/components/Handyperson_history.js
+++ client/src/components/Handyperson_history.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
 import "./handyperson_history.css";
 
 const HandypersonHistory = () => {
   const [todos, setTodos] = useState([]);
+  
+
   const getDataBase = async (e) => {
     try {
-      const response = await fetch("http://localhost:5000/hp/:id");
+      const response = await fetch(`http://localhost:5000/hp/${1}`);
       const jsonData = await response.json();
-      setTodos(jsonData)
+      setTodos(jsonData);
+      
     } catch (err) {
       console.log(err.message)
     }
   };
-
+  
   useEffect(() => {
     getDataBase();
   }, [])
+
   return (
     <div>
       <h1>Handyperson History</h1>
-      <button className="history"><a href="/handyperson">Back</a></button>
+      <button className="history"><Link to="/handyperson">Back</Link></button>
       <button className="Logout" onClick={() => window.location = "/#menu"}>
         Logout
       </button>
@@ -40,15 +45,15 @@ const HandypersonHistory = () => {
         <tbody>
           {todos.map(item => (
             <tr key={item.order_id}>
-              <td>{item.customers.first_name}</td>
-              <td>{item.customers.last_name}</td>
-              <td>{item.skills.skill_name}</td>
-              <td>{item.tasks.create_time}</td>
-              <td>{item.skills.price}</td>
-              <td>{item.tasks.description}</td>
-              <td>{item.tasks.address}</td>
-              <td>{item.customers.email}</td>
-              <td className={item.orders.status === "Done" ? "done" : ""}>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.skill_name}</td>
+              <td>{item.create_time}</td>
+              <td>${item.price}</td>
+              <td>{item.description}</td>
+              <td>{item.address}</td>
+              <td>{item.email}</td>
+              <td className={item.status === "Done" ? "done" : ""}>
                 {item.status}
               </td>
             </tr>))}
