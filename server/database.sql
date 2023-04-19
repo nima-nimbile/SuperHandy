d4c7171d9532fcb1a5aa69b77fc2c56e680e9bf9
Nima <nima.mohamed@hotmail.com>
2023-04-19 11:14:16 -0700

    connected database

--- server/database.sql
+++ server/database.sql
@@ -1,22 +1,29 @@
-DROP TABLE IF EXISTS order_assignment CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
 DROP TABLE IF EXISTS customers CASCADE;
 DROP TABLE IF EXISTS handypersons CASCADE;
+DROP TABLE IF EXISTS skills CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+
 
 CREATE TABLE customers (
-  customer_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
-  phone_number VARCHAR(20),
+  phone_number VARCHAR(20) NOT NULL,
   username VARCHAR(50) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  address VARCHAR(255)
+  address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE skills (
+  id SERIAL PRIMARY KEY NOT NULL,
+  skill_name VARCHAR(50) NOT NULL,
+  price DECIMAL(8,2) NOT NULL
 );
 
 CREATE TABLE handypersons (
-  handyperson_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
@@ -24,30 +31,38 @@ CREATE TABLE handypersons (
   username VARCHAR(50) NOT NULL,
   password VARCHAR(255) NOT NULL,
   address VARCHAR(255) NOT NULL,
-  skill INTEGER NOT NULL,
-  rating DECIMAL(3,2) NOT NULL
+  rating DECIMAL(3,2) DEFAULT 0.0 NOT NULL,
+  skill_id INTEGER REFERENCES skills(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  skill_id INTEGER REFERENCES skills(id) ON DELETE CASCADE
 );
 
 CREATE TABLE orders (
-  order_id SERIAL PRIMARY KEY NOT NULL,
-  customer_id INTEGER NOT NULL,
-  order_type_id INTEGER NOT NULL,
-  description_inquiry TEXT NOT NULL,
-  comment TEXT,
-  address VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(20) NOT NULL,
-  create_datetime TIMESTAMP DEFAULT NOW()
-);
-CREATE TABLE order_assignment (
-  assignment_id SERIAL PRIMARY KEY NOT NULL,
-  handyperson_id INTEGER NOT NULL,
-  order_id INTEGER NOT NULL,
-  status VARCHAR(20) NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  handyperson_id INTEGER REFERENCES handypersons(id) ON DELETE CASCADE,
+  task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE
 );
 
-CREATE TABLE categories (
-  category_id SERIAL PRIMARY KEY NOT NULL,
-  handyperson_id INTEGER NOT NULL,
-  price DECIMAL(6,2) NOT NULL,
-  category_name VARCHAR(50) NOT NULL
-);
+INSERT INTO customers (id, first_name, last_name, email, phone_number, username, password, address)
+VALUES (1, 'nima', 'mo', 'nima@gmail.com', '60488855522', 'nemo', '123', '406 Citadel');
+
+INSERT INTO handypersons (id, first_name, last_name, email, phone_number, username, password, address, skill_id)
+VALUES (1,'Jack', 'mp', 'jack@gmail.com', '6047778594', 'Jacki', '123', '555 skylin', 1);
+
+INSERT INTO skills (id, skill_name, price )
+VALUES (1, 'painter', '85');
+
+INSERT INTO tasks (id, description, address, create_time, customer_id, skill_id)
+VALUES (1, 'hello painter', '888 citadel', NOW(), 1, 1);
+
+INSERT INTO orders (id,status, handyperson_id, task_id )
+VALUES (1, 'pending', 1, 1);
--- server/index.js
+++ server/index.js
@@ -3,13 +3,65 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const db = require('./db');
+const { Pool } = require("pg");
 dotenv.config();
 // console.log(process.env.DB_PASS);
 
 //middleware
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
-app.listen(5000,()=>{
-  console.log("server has started on port 5000");
+//ROUTES//
+
+
+app.get("/hp/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const someInfo = await db.query(`
+      SELECT 
+        customers.first_name, 
+        customers.last_name, 
+        skills.skill_name, 
+        tasks.create_time, 
+        skills.price, 
+        tasks.description, 
+        tasks.address, 
+        customers.email, 
+        orders.status,
+        orders.id
+      FROM 
+        customers 
+      JOIN 
+        tasks ON customers.id = tasks.customer_id 
+      JOIN 
+        skills ON tasks.skill_id = skills.id 
+      JOIN 
+        orders ON tasks.id = orders.task_id 
+      WHERE
+        customers.id = $1
+    `, [id]); // Pass the id parameter as a query parameter
+    res.json(someInfo.rows);
+  } catch (error) {
+    // Handle error
+    console.error(error);
+  }
+});
+
+//edit request
+
+app.put("/edit/:id", async (req,res) => {
+  try {
+    const { id } = req.params;
+    const {status} = req.body;
+    const updatStatus = await db.query(`UPDATE orders SET status = $1 WHERE orders_id = $2`, 
+    [status, id]
+    );
+res.json(updatStatus)
+  } catch (error) {
+    console.error(error);
+  }
 })
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+})  
