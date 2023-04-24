e601e311755d210645bb5de3428d9afd42bd58ee
Nima <nima.mohamed@hotmail.com>
2023-04-24 12:16:39 -0700

    login and register style

--- client/src/components/HandyDash.js
+++ client/src/components/HandyDash.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom"
 import './handyDash.scss';
-import "./handyperson.css";
+import "./handyperson.scss";
 
 const HandyDash = (props) => {
   const [handyPerson, setHandyPerson] = useState(null);
